5575341fa6d17e9bfd48beb1429fb4acd442884f
Robin Radic <robin@radic.nl>
2017-05-29 19:05:51 +0200

    fix console command

--- src/Console/MetaCommand.php
+++ src/Console/MetaCommand.php
@@ -8,21 +8,34 @@
 
 namespace Laradic\Idea\Console;
 
-use Laradic\Console\Command;
+use Illuminate\Console\Command;
 use Laradic\Idea\Metadata\MetaRepository;
 use Laradic\Idea\Metadata\Seeder;
 
+/**
+ * This is the class MetaCommand.
+ *
+ * @package Laradic\Idea\Console
+ * @author  Robin Radic
+ */
 class MetaCommand extends Command
 {
+    /** @var string */
     protected $signature = 'idea:meta
                                      {--list : Lists all meta}
                                      {--exclude : Excludes meta}';
 
+    /** @var string */
     protected $description = 'Generates a .phpstorm.meta.php file with the configured metas';
 
+    /**
+     * handle method
+     *
+     * @return mixed
+     */
     public function handle()
     {
-        /** @var MetaRepository $metas */
+        /** @var \Laradic\Idea\Metadata\MetaRepository $metas */
         $metas = app('laradic.idea.meta');
         if ( $this->option('list') ) {
             return $this->listing(array_values($metas->all()));
